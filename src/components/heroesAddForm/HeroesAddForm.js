693668ba8ce9bfef84134b7f5677fbb5047590a9
molexandev01~ <molexandev@gmail.com>
2025-04-16 18:00:25 +0300

    added character deletion and addition

--- src/components/heroesAddForm/HeroesAddForm.js
+++ src/components/heroesAddForm/HeroesAddForm.js
@@ -1,5 +1,3 @@
-
-
 // Задача для этого компонента:
 // Реализовать создание нового героя с введенными данными. Он должен попадать
 // в общее состояние и отображаться в списке + фильтроваться
@@ -9,50 +7,99 @@
 // Дополнительно:
 // Элементы <option></option> желательно сформировать на базе
 // данных из фильтров
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useHttp } from '../../hooks/http.hook';
+import { heroesFetchingError, heroAdded } from '../../actions';
+import { v4 as uuidv4 } from 'uuid';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 const HeroesAddForm = () => {
-    return (
-        <form className="border p-4 shadow-lg rounded">
-            <div className="mb-3">
-                <label htmlFor="name" className="form-label fs-4">Имя нового героя</label>
-                <input 
-                    required
-                    type="text" 
-                    name="name" 
-                    className="form-control" 
-                    id="name" 
-                    placeholder="Как меня зовут?"/>
-            </div>
+   const [name, setName] = useState('');
+   const [description, setDescription] = useState('');
+   const [element, setElement] = useState('');
 
-            <div className="mb-3">
-                <label htmlFor="text" className="form-label fs-4">Описание</label>
-                <textarea
-                    required
-                    name="text" 
-                    className="form-control" 
-                    id="text" 
-                    placeholder="Что я умею?"
-                    style={{"height": '130px'}}/>
-            </div>
+   const dispatch = useDispatch();
+   const { request } = useHttp();
 
-            <div className="mb-3">
-                <label htmlFor="element" className="form-label">Выбрать элемент героя</label>
-                <select 
-                    required
-                    className="form-select" 
-                    id="element" 
-                    name="element">
-                    <option >Я владею элементом...</option>
-                    <option value="fire">Огонь</option>
-                    <option value="water">Вода</option>
-                    <option value="wind">Ветер</option>
-                    <option value="earth">Земля</option>
-                </select>
-            </div>
+   const onSubmitHandler = (e) => {
+      e.preventDefault();
 
-            <button type="submit" className="btn btn-primary">Создать</button>
-        </form>
-    )
-}
+      const newHero = {
+         id: uuidv4(),
+         name,
+         description,
+         element,
+      };
+
+      request(`http://localhost:3001/heroes`, 'POST', JSON.stringify(newHero))
+         .then(() => dispatch(heroAdded(newHero)))
+         .catch(() => dispatch(heroesFetchingError()));
+
+      setName('');
+      setDescription('');
+      setElement('');
+   };
+
+   return (
+      <form className="border p-4 shadow-lg rounded" onSubmit={onSubmitHandler}>
+         <div className="mb-3">
+            <label htmlFor="name" className="form-label fs-4">
+               Имя нового героя
+            </label>
+            <input
+               value={name}
+               onChange={(e) => setName(e.target.value)}
+               required
+               type="text"
+               name="name"
+               className="form-control"
+               id="name"
+               placeholder="Как меня зовут?"
+            />
+         </div>
+
+         <div className="mb-3">
+            <label htmlFor="text" className="form-label fs-4">
+               Описание
+            </label>
+            <textarea
+               value={description}
+               onChange={(e) => setDescription(e.target.value)}
+               required
+               name="description"
+               className="form-control"
+               id="text"
+               placeholder="Что я умею?"
+               style={{ height: '130px' }}
+            />
+         </div>
+
+         <div className="mb-3">
+            <label htmlFor="element" className="form-label">
+               Выбрать элемент героя
+            </label>
+            <select
+               value={element}
+               onChange={(e) => setElement(e.target.value)}
+               required
+               className="form-select"
+               id="element"
+               name="element"
+            >
+               <option>Я владею элементом...</option>
+               <option value="fire">Огонь</option>
+               <option value="water">Вода</option>
+               <option value="wind">Ветер</option>
+               <option value="earth">Земля</option>
+            </select>
+         </div>
+
+         <button type="submit" className="btn btn-primary">
+            Создать
+         </button>
+      </form>
+   );
+};
 
 export default HeroesAddForm;
