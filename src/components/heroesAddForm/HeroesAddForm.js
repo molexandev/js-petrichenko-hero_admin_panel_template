fbb8f9190a325d13f954c17f4e07652564e383c7
molexandev01~ <molexandev@gmail.com>
2025-05-01 16:33:38 +0300

    added RTK query and completed the project

--- src/components/heroesAddForm/HeroesAddForm.js
+++ src/components/heroesAddForm/HeroesAddForm.js
@@ -5,12 +5,14 @@ import { v4 as uuidv4 } from 'uuid';
 import store from '../../store';
 
 import { selectAll } from '../heroesFilters/filtersSlice';
-import { heroCreated } from '../heroesList/heroesSlice';
+import { useCreateHeroMutation } from '../../api/apiSlice';
 
 const HeroesAddForm = () => {
    const [heroName, setHeroName] = useState('');
    const [heroDescr, setHeroDescr] = useState('');
    const [heroElement, setHeroElement] = useState('');
+
+   const [createHero, { isLoading }] = useCreateHeroMutation();
 
    const { filtersLoadingStatus } = useSelector((state) => state.filters);
    const filters = selectAll(store.getState());
@@ -26,10 +28,9 @@ const HeroesAddForm = () => {
          element: heroElement,
       };
 
-      request('http://localhost:3001/heroes', 'POST', JSON.stringify(newHero))
-         .then((res) => console.log(res, 'Отправка успешна'))
-         .then(dispatch(heroCreated(newHero)))
-         .catch((err) => console.log(err));
+      createHero(newHero)
+         .unwrap()
+         .then((res) => console.log(res));
 
       setHeroName('');
       setHeroDescr('');
